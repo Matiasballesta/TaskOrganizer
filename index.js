6e0ea407fb6aab2d34dd121ff978489c4e322a2b
Matiasballesta <75946070+Matiasballesta@users.noreply.github.com>
2022-10-26 14:17:23 -0300

    Cors fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDb from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import proyectoRoutes from "./routes/proyectoRoutes.js"
@@ -14,6 +15,20 @@ const PORT = process.env.PORT || 4000;
 
 connectDb();
 
+//Configurar Cors
+const whitelist = ['http://127.0.0.1:5173'];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(whitelist.includes(origin)){
+      callback(null, true) 
+    }else{
+      callback(new Error('Error de cors'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 //Routing
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
